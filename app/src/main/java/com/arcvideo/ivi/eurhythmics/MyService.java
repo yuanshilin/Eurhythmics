92121ac56aeed379b7859c858f0388a1acd58399
feixiao <feixiao2020@sina.com>
2024-02-29 15:45:09 +0800

    update

--- app/src/main/java/com/arcvideo/ivi/eurhythmics/MyService.java
+++ app/src/main/java/com/arcvideo/ivi/eurhythmics/MyService.java
@@ -5,9 +5,19 @@ import android.content.Intent;
 import android.os.IBinder;
 import com.tencent.mars.xlog.Log;
 
+import com.arcvideo.ivi.light.musical.Factory;
+import com.arcvideo.ivi.light.musical.Listener;
+import com.arcvideo.ivi.light.musical.NativeVisualizer;
+import com.arcvideo.ivi.light.musical.Visualizer;
+import com.arcvideo.ivi.light.musical.VisualizerType;
+import com.arcvideo.ivi.light.utils.RGB;
+
+
 public class MyService extends Service {
 
     private static final String TAG = "Eurhythmics";
+
+    private Visualizer _visualizer;
     @Override
     public IBinder onBind(Intent intent) {
         // 如果服务不支持绑定，可以返回 null
@@ -20,6 +30,14 @@ public class MyService extends Service {
         super.onCreate();
         // Start the service
         Log.d(TAG, "onCreate the service");
+        if (_visualizer == null) {
+            _visualizer  = Factory.create(VisualizerType.NATIVE, 0, new Listener() {
+                @Override
+                public void onColor(RGB rgb) {
+                    Log.d(TAG, "onColor: " + rgb);
+                }
+            });;
+        }
     }
 
     @Override
@@ -27,5 +45,9 @@ public class MyService extends Service {
         super.onDestroy();
         // Stop the service
         Log.d(TAG, "onDestroy the service");
+        if (_visualizer != null) {
+            _visualizer.destroy();
+            _visualizer = null;
+        }
     }
 }
