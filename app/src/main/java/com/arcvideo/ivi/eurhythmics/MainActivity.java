269a47c61e662b5b02bcaef8813d66b33796866d
feixiao <feixiao2020@sina.com>
2024-02-29 14:48:45 +0800

    添加依赖项

--- app/src/main/java/com/arcvideo/ivi/eurhythmics/MainActivity.java
+++ app/src/main/java/com/arcvideo/ivi/eurhythmics/MainActivity.java
@@ -3,12 +3,35 @@ package com.arcvideo.ivi.eurhythmics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.arcvideo.ivi.light.utils.MarsLog;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnStart;
+    private Button btnStop;
+
+    private static final String TAG = "Eurhythmics";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MarsLog.init("","",7);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(v -> {
+            // Start the service
+            Log.d(TAG, "Start the service");
+        });
+
+        btnStop.setOnClickListener(v -> {
+            // Stop the service
+            Log.d(TAG, "Stop the service");
+        });
     }
 }
